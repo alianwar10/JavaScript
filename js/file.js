16136565584050b1e2170a62d3eac221d1092ae8
Hamzah <hamzalianwar@hotmail.com>
2025-02-01 18:44:40 -0500

    JS bootcamp

---
+++ js/file.js
@@ -0,0 +1,5 @@
+const arr1 = [1, 2, 3, 4, 5]
+console.log(arr1)
+
+arr1.push(6)
+console.log(arr1)
---
+++ js/index.js
@@ -0,0 +1,8 @@
+console.time('You code took')
+console.log('Welcome to JS');
+// console.clear();
+console.table({hamza: 'this', marks: 85});
+console.warn('This is a warning');
+console.timeEnd('Your code took')
+// console.assert(age>189, 'This is not possible');
+console.error('This is an erro');
