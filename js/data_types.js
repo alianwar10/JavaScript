0369da2594d3e04c1f77bafe081d7f4c7b4e9e1c
Hamzah <hamzalianwar@hotmail.com>
2025-02-01 19:44:44 -0500

    JS bootcamp

---
+++ js/data_types.js
@@ -0,0 +1,48 @@
+// Primitive data types
+
+let name = "Harry";
+
+console.log("My string is " + name);
+console.log("Data type is " + (typeof name));
+
+let marks = 34;
+
+console.log("Data type is: " + (typeof marks));
+
+let isDriver = true;
+
+console.log("Data type is: " + (typeof isDriver));
+
+let nullVar = null;
+
+console.log("Data type is: " + (typeof nullVar));
+
+let undef = undefined;
+
+console.log("Data type is: " + (typeof undef));
+
+// Reference data type
+
+// Arrays
+myarr = [1, 2, 3, 4, "string", true];
+console.log("Data type is: " + (typeof myarr));
+
+// Object Literals
+let stMarks = {
+    harry: 89,
+    Nawaz: 78,
+    Altaf: 86
+};
+
+console.log("Data type is: " + (typeof stMarks));
+
+// Functions
+function findName() {
+
+}
+
+console.log("Data type is: " + (typeof findName))
+
+// Date
+let date = new Date();
+console.log("Data type is: " + (typeof Date))
